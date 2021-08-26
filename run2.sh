8fd2365d2681ecde9063175210985dab27c3fc0e
XanibX <89570919+XanibX@users.noreply.github.com>
2021-08-26 10:55:26 +0300

    Create run2.sh

---
+++ run2.sh
@@ -0,0 +1,10 @@
+wget -O w10x64.img https://bit.ly/akuhnetW10x64
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+read -p "Ctrl + V Authtoken(nhập token ngrok): " CRP 
+./ngrok authtoken $CRP 
+nohup ./ngrok tcp 3388 &>/dev/null &
+apt-get install qemu
+curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
+qemu-system-x86_64 -hda w10x64.img -m 8G -smp cores=4 -net user,hostfwd=tcp::3388-:3389 -net nic -object rng-random,id=rng0,filename=/dev/urandom -device virtio-rng-pci,rng=rng0 -vga vmware -nographic
+sleep 43200
